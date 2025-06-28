a9e91ff4aa157c0f63f91bcfe54e1b526be68c6e
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-06-28 15:38:49 -0300

    chore: init

---
+++ src/main/java/com/skillvault/backend/SkillVaultApplication.java
@@ -0,0 +1,13 @@
+package com.skillvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillVaultApplication.class, args);
+	}
+
+}
