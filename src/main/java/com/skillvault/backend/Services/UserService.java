d7a742c3d5dfbb60223259fd04ca525e3694f194
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-06-29 17:52:57 -0300

    fix: load user by username now is returning user;

--- src/main/java/com/skillvault/backend/Services/UserService.java
+++ src/main/java/com/skillvault/backend/Services/UserService.java
@@ -1,14 +1,20 @@
 package com.skillvault.backend.Services;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import com.skillvault.backend.Domain.User;
+import com.skillvault.backend.Repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow();
     }
 }
